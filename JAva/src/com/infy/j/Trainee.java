3b89a53ad77c5b1bafb68ff387fd7eec8d97bfdf
Ganesh <gkasar39@gmail.com>
2022-03-14 17:28:26 +0530

    new change applied

--- JAva/src/com/infy/j/Trainee.java
+++ JAva/src/com/infy/j/Trainee.java
@@ -6,12 +6,13 @@ class Employee{
 }
 
 public class Trainee extends Employee {
-	public static void display(){
-		System.out.println("Trainee");
-	}
 	public static void main(String[] args) {
-		Employee employee = new Trainee();
-		employee.display();
+		System.out.println("Welcome");
+	
+		/*
+		 * public static void display(){ System.out.println("Trainee"); } public static
+		 * void main(String[] args) { Employee employee = new Trainee();
+		 * employee.display(); }
+		 */
 	}
-
 }
